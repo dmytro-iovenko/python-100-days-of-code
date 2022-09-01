ca294e117a64c92f03075d6bfe00105af4433c05
liudmylai <liudmyla.iovenko@gmail.com>
2022-08-31 21:24:07 -0400

    adding car behaviour

--- 23-turtle-crossing/main.py
+++ 23-turtle-crossing/main.py
@@ -8,7 +8,20 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 
+player = Player()
+car_manager = CarManager()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(player.go_up, "Up")
+
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+
+    car_manager.create_car()
+    car_manager.move_cars()
+
+
+screen.exitonclick()
