bfb61572025c0ce2495d361639d4b08e0ddc8702
liudmylai <liudmyla.iovenko@gmail.com>
2022-09-26 21:50:14 -0400

    using Sheety to read and write data to Google Sheet

--- 39-flight-deals/README.md
+++ 39-flight-deals/README.md
@@ -1,1 +1,5 @@
 ## Day 39 Project - Flight Deal Finder
+
+### Learning Objectives
+* Practice using REST API
+* Practice saving data into Google Sheets with Sheety
--- 39-flight-deals/data_manager.py
+++ 39-flight-deals/data_manager.py
@@ -1,3 +1,31 @@
+import requests
+
+SHEETY_PRICES_ENDPOINT = YOUR ENDPOINT HERE
+
+
 class DataManager:
-    #This class is responsible for talking to the Google Sheet.
-    pass
+
+    def __init__(self):
+        self.destination_data = {}
+
+    def get_destination_data(self):
+        # Use the Sheety API to GET all the data in that sheet and print it out.
+        response = requests.get(url=SHEETY_PRICES_ENDPOINT)
+        data = response.json()
+        self.destination_data = data["prices"]
+        return self.destination_data
+
+    # Make a PUT request and use the row id from sheet_data
+    # to update the Google Sheet with the IATA codes.
+    def update_destination_codes(self):
+        for city in self.destination_data:
+            new_data = {
+                "price": {
+                    "iataCode": city["iataCode"]
+                }
+            }
+            response = requests.put(
+                url=f"{SHEETY_PRICES_ENDPOINT}/{city['id']}",
+                json=new_data
+            )
+            print(response.text)
