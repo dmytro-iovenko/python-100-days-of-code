de3abf7ffa82049d767de273a08dc244e9c2bb3a
liudmylai <liudmyla.iovenko@gmail.com>
2022-09-27 22:54:32 -0400

    getting IATA codes using Kiwi Partners API

--- 39-flight-deals/flight_search.py
+++ 39-flight-deals/flight_search.py
@@ -1,12 +1,16 @@
 import requests
 
 TEQUILA_ENDPOINT = "https://tequila-api.kiwi.com"
-TEQUILA_API_KEY = "YOUR_API_KEY_HERE"
+TEQUILA_API_KEY = YOUR FLIGHT SEARCH API KEY
 
 
 class FlightSearch:
 
     def get_destination_code(self, city_name):
-        # Return "TESTING" for now to make sure Sheety is working. Get TEQUILA API data later.
-        code = "TESTING"
+        location_endpoint = f"{TEQUILA_ENDPOINT}/locations/query"
+        headers = {"apikey": TEQUILA_API_KEY}
+        query = {"term": city_name, "location_types": "city"}
+        response = requests.get(url=location_endpoint, headers=headers, params=query)
+        results = response.json()["locations"]
+        code = results[0]["code"]
         return code
