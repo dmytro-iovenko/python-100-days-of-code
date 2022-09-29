a265ccace45aae2b159b5a885707f5e1f118cab1
liudmylai <liudmyla.iovenko@gmail.com>
2022-09-28 21:33:07 -0400

    searching for flights

--- 39-flight-deals/data_manager.py
+++ 39-flight-deals/data_manager.py
@@ -1,6 +1,7 @@
+from pprint import pprint
 import requests
 
-SHEETY_PRICES_ENDPOINT = YOUR ENDPOINT HERE
+SHEETY_PRICES_ENDPOINT = YOUR SHEETY PRICES ENDPOINT
 
 
 class DataManager:
@@ -9,14 +10,11 @@ class DataManager:
         self.destination_data = {}
 
     def get_destination_data(self):
-        # Use the Sheety API to GET all the data in that sheet and print it out.
         response = requests.get(url=SHEETY_PRICES_ENDPOINT)
         data = response.json()
         self.destination_data = data["prices"]
         return self.destination_data
 
-    # Make a PUT request and use the row id from sheet_data
-    # to update the Google Sheet with the IATA codes.
     def update_destination_codes(self):
         for city in self.destination_data:
             new_data = {
