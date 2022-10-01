fda165d3b739df22b8f31bd359d5f17bea0d013c
liudmylai <liudmyla.iovenko@gmail.com>
2022-09-30 22:17:58 -0400

    exception handling

--- 39-flight-deals/main.py
+++ 39-flight-deals/main.py
@@ -26,6 +26,10 @@ for destination in sheet_data:
         from_time=tomorrow,
         to_time=six_month_from_today
     )
+
+    if flight is None:
+        continue
+
     if flight.price < destination["lowestPrice"]:
         notification_manager.send_sms(
             message=f"Low price alert! Only £{flight.price} to fly from {flight.origin_city}-{flight.origin_airport} to {flight.destination_city}-{flight.destination_airport}, from {flight.out_date} to {flight.return_date}."
