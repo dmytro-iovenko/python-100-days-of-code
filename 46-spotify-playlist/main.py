78204750154791b0f55c21e523d823719f8f8012
liudmylai <liudmyla.iovenko@gmail.com>
2022-10-09 19:32:39 -0400

    authentication with Spotify

--- 46-spotify-playlist/main.py
+++ 46-spotify-playlist/main.py
@@ -1,10 +1,14 @@
-from bs4 import BeautifulSoup
-import requests
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
 
-date = input("Which year do you want to travel to? Type the date in this format YYYY-MM-DD: ")
-
-response = requests.get("https://www.billboard.com/charts/hot-100/" + date)
-
-soup = BeautifulSoup(response.text, 'html.parser')
-song_names_spans = soup.find_all("span", class_="chart-element__information__song")
-song_names = [song.getText() for song in song_names_spans]
+sp = spotipy.Spotify(
+    auth_manager=SpotifyOAuth(
+        scope="playlist-modify-private",
+        redirect_uri="http://example.com",
+        client_id=YOUR UNIQUE CLIENT ID,
+        client_secret=YOUR UNIQUE CLIENT SECRET,
+        show_dialog=True,
+        cache_path="token.txt"
+    )
+)
+user_id = sp.current_user()["id"]
