dcb5f46d4d2bab144be71f221b1bd3bf46e2704f
liudmylai <liudmyla.iovenko@gmail.com>
2022-10-08 20:48:12 -0400

    scraping Billboard Hot 100

---
+++ 46-spotify-playlist/main.py
@@ -0,0 +1,10 @@
+from bs4 import BeautifulSoup
+import requests
+
+date = input("Which year do you want to travel to? Type the date in this format YYYY-MM-DD: ")
+
+response = requests.get("https://www.billboard.com/charts/hot-100/" + date)
+
+soup = BeautifulSoup(response.text, 'html.parser')
+song_names_spans = soup.find_all("span", class_="chart-element__information__song")
+song_names = [song.getText() for song in song_names_spans]
