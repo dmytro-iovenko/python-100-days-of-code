eedf77f918c4de4c3fd145df7aef52621f3a3cba
liudmylai <liudmyla.iovenko@gmail.com>
2022-10-17 20:07:59 -0400

    initial commit

---
+++ 50-auto-tinder-swiper/main.py
@@ -0,0 +1,18 @@
+from selenium import webdriver
+from time import sleep
+
+FB_EMAIL = YOUR FACEBOOK LOGIN EMAIL
+FB_PASSWORD = YOUR FACEBOOK PASSWORD
+
+chrome_driver_path = YOUR CHROME DRIVER PATH
+driver = webdriver.Chrome(executable_path=chrome_driver_path)
+
+driver.get("http://www.tinder.com")
+
+sleep(2)
+login_button = driver.find_element_by_xpath('//*[@id="content"]/div/div[1]/div/main/div[1]/div/div/header/div[1]/div[2]/div/button')
+login_button.click()
+
+sleep(2)
+fb_login = driver.find_element_by_xpath('//*[@id="modal-manager"]/div/div/div[1]/div/div[3]/span/div[2]/button')
+fb_login.click()
