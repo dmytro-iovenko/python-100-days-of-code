6dc70463a52ae47e67d81bb7e474d4a8db153f6d
liudmylai <liudmyla.iovenko@gmail.com>
2022-10-20 21:09:24 -0400

    hit like

--- 50-auto-tinder-swiper/main.py
+++ 50-auto-tinder-swiper/main.py
@@ -50,3 +50,27 @@ notifications_button.click()
 #Allow cookies
 cookies = driver.find_element_by_xpath('//*[@id="content"]/div/div[2]/div/div/div[1]/button')
 cookies.click()
+
+#Tinder free tier only allows 100 "Likes" per day.
+for n in range(100):
+
+    #Add a 1 second delay between likes.
+    sleep(1)
+
+    try:
+        print("called")
+        like_button = driver.find_element_by_xpath(
+            '//*[@id="content"]/div/div[1]/div/main/div[1]/div/div/div[1]/div/div[2]/div[4]/button')
+        like_button.click()
+
+    #Catches the cases where there is a "Matched" pop-up in front of the "Like" button:
+    except ElementClickInterceptedException:
+        try:
+            match_popup = driver.find_element_by_css_selector(".itsAMatch a")
+            match_popup.click()
+
+        #Catches the cases where the "Like" button has not yet loaded, so wait 2 seconds before retrying.
+        except NoSuchElementException:
+            sleep(2)
+
+driver.quit()
