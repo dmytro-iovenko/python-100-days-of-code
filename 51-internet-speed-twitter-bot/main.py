68b2971dd252f31fdbcc579628c5f5be9de041fd
liudmylai <liudmyla.iovenko@gmail.com>
2022-10-22 23:09:26 -0400

    get internet speed

--- 51-internet-speed-twitter-bot/main.py
+++ 51-internet-speed-twitter-bot/main.py
@@ -14,11 +14,21 @@ class InternetSpeedTwitterBot:
         self.down = 0
 
     def get_internet_speed(self):
-        pass
+        self.driver.get("https://www.speedtest.net/")
+    
+        time.sleep(3)
+        go_button = self.driver.find_element_by_css_selector(".start-button a")
+        go_button.click()
+    
+        time.sleep(60)
+        self.up = self.driver.find_element_by_xpath('//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[3]/div/div/div[2]/div[1]/div[2]/div/div[2]/span').text
+        self.down = self.driver.find_element_by_xpath('//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[3]/div/div/div[2]/div[1]/div[3]/div/div[2]/span').text
 
     def tweet_at_provider(self):
         pass
 
+
 bot = InternetSpeedTwitterBot(CHROME_DRIVER_PATH)
 bot.get_internet_speed()
 bot.tweet_at_provider()
+
