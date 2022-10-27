9af7e799d8d12455a282a9bdfaad56ff09f8c699
liudmylai <liudmyla.iovenko@gmail.com>
2022-10-26 22:29:17 -0400

    finding the follower

--- 52-instagram-follower/main.py
+++ 52-instagram-follower/main.py
@@ -27,7 +27,18 @@ class InstaFollower:
         password.send_keys(Keys.ENTER)
 
     def find_followers(self):
-        pass
+        time.sleep(5)
+        self.driver.get(f"https://www.instagram.com/{SIMILAR_ACCOUNT}")
+    
+        time.sleep(2)
+        followers = self.driver.find_element_by_xpath('//*[@id="react-root"]/section/main/div/header/section/ul/li[2]/a')
+        followers.click()
+    
+        time.sleep(2)
+        modal = self.driver.find_element_by_xpath('/html/body/div[4]/div/div/div[2]')
+        for i in range(10):
+            self.driver.execute_script("arguments[0].scrollTop = arguments[0].scrollHeight", modal)
+            time.sleep(2)
 
     def follow(self):
         pass
