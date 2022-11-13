8e233255a07a624c3dd029560cac97fe8275b8d8
liudmylai <liudmyla.iovenko@gmail.com>
2022-11-12 20:51:45 -0500

    rendering blog posts from api

--- 59-upgraded-blog/main.py
+++ 59-upgraded-blog/main.py
@@ -1,12 +1,21 @@
 from flask import Flask, render_template
+import requests
+
+posts = requests.get("https://api.npoint.io/f32056f65dabeff2da02").json()
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def get_all_posts():
-    return render_template("index.html")
+    return render_template("index.html", all_posts=posts)
 
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
