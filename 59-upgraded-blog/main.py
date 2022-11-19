2277b4dbbae63b69707ee210323243a91972a116
liudmylai <liudmyla.iovenko@gmail.com>
2022-11-19 18:07:12 -0500

    sending email with smtplib

--- 59-upgraded-blog/main.py
+++ 59-upgraded-blog/main.py
@@ -1,7 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import smtplib
 import requests
 
 posts = requests.get("https://api.npoint.io/f32056f65dabeff2da02").json()
+MY_EMAIL = YOUR OWN EMAIL ADDRESS
+MY_PASSWORD = YOUR OWN EMAIL PASSWORD
 
 app = Flask(__name__)
 
@@ -25,12 +28,16 @@ def show_post(index):
 def contact():
     if request.method == "POST":
         data = request.form
-        print(data["name"])
-        print(data["email"])
-        print(data["phone"])
-        print(data["message"])
+        send_email(data["name"], data["email"], data["phone"], data["message"])
         return render_template("contact.html", msg_sent=True)
     return render_template("contact.html", msg_sent=False)
+
+def send_email(name, email, phone, message):
+    email_message = f"Subject:New Message\n\nName: {name}\nEmail: {email}\nPhone: {phone}\nMessage:{message}"
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(MY_EMAIL, MY_PASSWORD)
+        connection.sendmail(MY_EMAIL, MY_EMAIL, email_message)
 
 if __name__ == "__main__":
     app.run(debug=True)
