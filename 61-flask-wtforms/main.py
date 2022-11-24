8925265a6625bc854792f00f908f92a659953b8f
liudmylai <liudmyla.iovenko@gmail.com>
2022-11-24 18:45:53 -0500

    receiving form data

--- 61-flask-wtforms/main.py
+++ 61-flask-wtforms/main.py
@@ -17,7 +17,11 @@ def home():
 @app.route("/login", method=["GET", "POST"])
 def login():
     login_form = LoginForm()
-    login_form.validate_on_submit()
+    if login_form.validate_on_submit():
+        if login_form.email.data == "myemail@example.com" and login_form.password.data == "SeCrEtPaSsWoRd":
+            return render_template("success.html")
+        else:
+            return render_template("denied.html")
     return render_template("login.html", form=login_form)
 
 if __name__ == '__main__':
