c6ad57db29b94334476de9fac626e97553f5ce93
liudmylai <liudmyla.iovenko@gmail.com>
2022-12-02 21:39:09 -0500

    using sqlite database

--- 63-virtual-library/main.py
+++ 63-virtual-library/main.py
@@ -1,28 +1,47 @@
 from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-all_books = []
+##CREATE DATABASE
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///books.db"
+#Optional: But it will silence the deprecation warning in the console.
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+##CREATE TABLE
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    author = db.Column(db.String(250), nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+
+
+db.create_all()
 
 
 @app.route('/')
 def home():
-    return render_template("index.html", books=all_books)
+    ##READ ALL RECORDS
+    all_books = db.session.query(Book).all()
+    return render_template("index.html", books=all_books)  
 
 
 @app.route("/add", methods=["GET", "POST"])
 def add():
     if request.method == "POST":
-        new_book = {
-            "title": request.form["title"],
-            "author": request.form["author"],
-            "rating": request.form["rating"]
-        }
-        all_books.append(new_book)
+        # CREATE RECORD
+        new_book = Book(
+            title=request.form["title"],
+            author=request.form["author"],
+            rating=request.form["rating"]
+        )
+        db.session.add(new_book)
+        db.session.commit()
         return redirect(url_for('home'))
-      
     return render_template("add.html")
 
-
+  
 if __name__ == "__main__":
     app.run(debug=True)
