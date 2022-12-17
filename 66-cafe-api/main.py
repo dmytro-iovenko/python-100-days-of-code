b067d83f0ed16b590ade898c19575b67b1a46287
liudmylai <liudmyla.iovenko@gmail.com>
2022-12-16 19:40:49 -0500

    deleting a cafe

--- 66-cafe-api/main.py
+++ 66-cafe-api/main.py
@@ -89,12 +89,24 @@ def patch_new_price(cafe_id):
     if cafe:
         cafe.coffee_price = new_price
         db.session.commit()
-        return jsonify(response={"success": "Successfully updated the price."})
+        return jsonify(response={"success": "Successfully updated the price."}), 200
     else:
-        return jsonify(error={"Not Found": "Sorry a cafe with that id was not found in the database."})
+        return jsonify(error={"Not Found": "Sorry a cafe with that id was not found in the database."}), 404
 
 ## HTTP DELETE - Delete Record
-
+@app.route("/report-closed/<int:cafe_id>", methods=["DELETE"])
+def delete_cafe(cafe_id):
+    api_key = request.args.get("api-key")
+    if api_key == "TopSecretAPIKey":
+        cafe = db.session.query(Cafe).get(cafe_id)
+        if cafe:
+            db.session.delete(cafe)
+            db.session.commit()
+            return jsonify(response={"success": "Successfully deleted the cafe from the database."}), 200
+        else:
+            return jsonify(error={"Not Found": "Sorry a cafe with that id was not found in the database."}), 404
+    else:
+        return jsonify(error={"Forbidden": "Sorry, that's not allowed. Make sure you have the correct api_key."}), 403
 
 if __name__ == '__main__':
     app.run(debug=True)
