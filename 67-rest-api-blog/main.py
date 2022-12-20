42e6a0dec36b52e0d232859f121c48d714557bc0
liudmylai <liudmyla.iovenko@gmail.com>
2022-12-19 21:30:33 -0500

    getting blog items

--- 67-rest-api-blog/main.py
+++ 67-rest-api-blog/main.py
@@ -37,17 +37,17 @@ class CreatePostForm(FlaskForm):
     submit = SubmitField("Submit Post")
 
 
+##RENDER HOME PAGE USING DB
 @app.route('/')
 def get_all_posts():
+    posts = BlogPost.query.all()
     return render_template("index.html", all_posts=posts)
 
 
-@app.route("/post/<int:index>")
-def show_post(index):
-    requested_post = None
-    for blog_post in posts:
-        if blog_post["id"] == index:
-            requested_post = blog_post
+##RENDER POST USING DB
+@app.route("/post/<int:post_id>")
+def show_post(post_id):
+    requested_post = BlogPost.query.get(post_id)
     return render_template("post.html", post=requested_post)
 
 
