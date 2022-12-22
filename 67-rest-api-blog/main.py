b5b86bd16e63d464efc7acea5eeb9b9d819d1ed9
liudmylai <liudmyla.iovenko@gmail.com>
2022-12-21 21:46:26 -0500

    auto-populate fields

--- 67-rest-api-blog/main.py
+++ 67-rest-api-blog/main.py
@@ -71,6 +71,19 @@ def add_new_post():
     return render_template("make-post.html", form=form)
 
 
+@app.route("/edit-post/<int:post_id>", methods=["GET", "POST"])
+def edit_post(post_id):
+    post = BlogPost.query.get(post_id)
+    edit_form = CreatePostForm(
+        title=post.title,
+        subtitle=post.subtitle,
+        img_url=post.img_url,
+        author=post.author,
+        body=post.body
+    )
+    return render_template("make-post.html", form=edit_form, is_edit=True)
+
+
 @app.route("/about")
 def about():
     return render_template("about.html")
