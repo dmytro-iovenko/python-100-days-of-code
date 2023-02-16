dd4fd9fedd0e77d36fff70f1aeecd64e42b42b10
liudmylai <liudmyla.iovenko@gmail.com>
2023-02-15 22:51:34 -0500

    show image in frame

--- 84-image-watermark/main.py
+++ 84-image-watermark/main.py
@@ -1,8 +1,14 @@
 from tkinter import *
-
+from PIL import Image, ImageTk
 
 window = Tk()
 window.title("Image Watermarking Desktop App")
 
+#Create an Image Object from an Image
+load = Image.open('dog.jpg')
+render = ImageTk.PhotoImage(load)
+img = Label(image=render)
+img.image = render
+img.place(x=0, y=0)
 
 window.mainloop()
