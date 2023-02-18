d6d716ba8286199afa36991155eeafd9ad013571
liudmylai <liudmyla.iovenko@gmail.com>
2023-02-17 21:41:40 -0500

    upload image by filedialog

--- 84-image-watermark/main.py
+++ 84-image-watermark/main.py
@@ -9,6 +9,12 @@ def uploadImg():
     image = Image.open(filename)
     # resize the image using resize() method        
     resize_image = image.resize((640, 400))
+
+    show_img = ImageTk.PhotoImage(resize_image)
+    # show the resized image
+    img.config(image=show_img)
+    img.image = show_img
+
     # save the image data into var_photo
     outfile = BytesIO()
     resize_image.save(outfile, "PNG")
@@ -49,15 +55,15 @@ im.save(byte_io, 'PNG')
 img_frame = LabelFrame(window, text="")
 img_frame.place(x = margin, y = 40 + margin * 2, width = width, height = height)
 
+# create the image inside img_frame
+img = Label(img_frame)
+img.pack()
+
 if var_image.get():
     # render watermarked image
     render = ImageTk.PhotoImage(data=var_image.get())
     img.config(image = render)
     img.image = render
-
-# create the image inside img_frame
-img = Label(img_frame)
-img.pack()
 
 btn_upload_img = Button(text = "Upload Image", command = uploadImg).place(x = margin, y = margin, width = 150, height = 40)
 #btn_save = Button( text="Save", bg="green", command=add).place(x=200, y=330, width= 150 , height=40)
