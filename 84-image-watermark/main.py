8aa0ad9a9a3bf5dede007fe22052205f52420958
liudmylai <liudmyla.iovenko@gmail.com>
2023-02-17 21:30:49 -0500

    upload image by filedialog

--- 84-image-watermark/main.py
+++ 84-image-watermark/main.py
@@ -1,9 +1,31 @@
 from tkinter import *
+from tkinter import filedialog
 from PIL import Image, ImageTk, ImageDraw, ImageFont
 from io import BytesIO
 
+def uploadImg():
+    # read the image
+    filename = filedialog.askopenfilename(initialdir =  "/", title = "Select an Image", filetype = (("jpeg files","*.jpg"),("PNG  files","*.png")))
+    image = Image.open(filename)
+    # resize the image using resize() method        
+    resize_image = image.resize((640, 400))
+    # save the image data into var_photo
+    outfile = BytesIO()
+    resize_image.save(outfile, "PNG")
+    var_image.set(outfile.getvalue())            
+
+# create window
+window = Tk()
+window.title('Image Watermarking Desktop App')
+
+# set window size
+window.geometry("660x470")
+
+var_image = Variable()  # use Variable() to store image
+
 #Create an Image Object from an Image
 im = Image.open('dog.jpg')
+im = im.resize((640, 400)) # Resize the image using resize() method
 width, height = im.size
 
 draw = ImageDraw.Draw(im)
@@ -24,17 +46,21 @@ draw.text((x, y), text, font=font)
 byte_io = BytesIO()
 im.save(byte_io, 'PNG')
 
-# create window
-window = Tk()
-window.title('Image Watermarking Desktop App')
+img_frame = LabelFrame(window, text="")
+img_frame.place(x = margin, y = 40 + margin * 2, width = width, height = height)
 
-# set window size
-window.geometry("%sx%s" % (width, height))
+if var_image.get():
+    # render watermarked image
+    render = ImageTk.PhotoImage(data=var_image.get())
+    img.config(image = render)
+    img.image = render
 
-# render watermarked image
-render = ImageTk.PhotoImage(Image.open(byte_io))
-img = Label(image=render)
-img.image = render
-img.place(x=0, y=0)
+# create the image inside img_frame
+img = Label(img_frame)
+img.pack()
+
+btn_upload_img = Button(text = "Upload Image", command = uploadImg).place(x = margin, y = margin, width = 150, height = 40)
+#btn_save = Button( text="Save", bg="green", command=add).place(x=200, y=330, width= 150 , height=40)
+
 
 window.mainloop()
