4f1ee8106191c7eb435b46e72bd360a0f0f4168d
liudmylai <liudmyla.iovenko@gmail.com>
2023-02-18 20:49:57 -0500

    add watermark to the image

--- 84-image-watermark/main.py
+++ 84-image-watermark/main.py
@@ -3,22 +3,55 @@ from tkinter import filedialog
 from PIL import Image, ImageTk, ImageDraw, ImageFont
 from io import BytesIO
 
+width, height, margin = 640, 400, 10
+
 def uploadImg():
     # read the image
     filename = filedialog.askopenfilename(initialdir =  "/", title = "Select an Image", filetype = (("jpeg files","*.jpg"),("PNG  files","*.png")))
-    image = Image.open(filename)
-    # resize the image using resize() method        
-    resize_image = image.resize((640, 400))
+    if filename:
+        image = Image.open(filename)
+        # resize the image using resize() method        
+        resize_image = image.resize((640, 400))
 
-    show_img = ImageTk.PhotoImage(resize_image)
-    # show the resized image
-    img.config(image=show_img)
-    img.image = show_img
+        show_img = ImageTk.PhotoImage(resize_image)
+        # show the resized image
+        img.config(image=show_img)
+        img.image = show_img
 
-    # save the image data into var_photo
-    outfile = BytesIO()
-    resize_image.save(outfile, "PNG")
-    var_image.set(outfile.getvalue())            
+        # save the image data into var_photo
+        outfile = BytesIO()
+        resize_image.save(outfile, "PNG")
+        outfile.seek(0)
+        byteImg = outfile.read()
+        var_image.set(byteImg)            
+
+        btn_set_watermark.config(state="normal")
+
+def setWatermark():
+    if var_image.get():
+        im = Image.open(BytesIO(var_image.get()))
+
+        draw = ImageDraw.Draw(im)
+        width, height = im.size
+
+        text = 'sample watermark'
+        font = ImageFont.truetype('arial.ttf', 36)
+        textwidth, textheight = draw.textsize(text, font)
+
+        # calculate the x,y coordinates of the text
+        x = width - textwidth - margin
+        y = height - textheight - margin
+
+        # draw watermark in the bottom right corner
+        draw.text((x, y), text, font=font)
+
+        # render the watermarked image
+        render = ImageTk.PhotoImage(im)
+
+        # show the watermarked image
+        img.config(image=render)
+        img.image = render
+
 
 # create window
 window = Tk()
@@ -28,29 +61,7 @@ window.title('Image Watermarking Desktop App')
 window.geometry("660x470")
 
 var_image = Variable()  # use Variable() to store image
-
-#Create an Image Object from an Image
-im = Image.open('dog.jpg')
-im = im.resize((640, 400)) # Resize the image using resize() method
-width, height = im.size
-
-draw = ImageDraw.Draw(im)
-text = 'sample watermark'
-
-font = ImageFont.truetype('arial.ttf', 36)
-textwidth, textheight = draw.textsize(text, font)
-
-# calculate the x,y coordinates of the text
-margin = 10
-x = width - textwidth - margin
-y = height - textheight - margin
-
-# draw watermark in the bottom right corner
-draw.text((x, y), text, font=font)
-
-# save watermarked image
-byte_io = BytesIO()
-im.save(byte_io, 'PNG')
+var_watermark = Variable()  # use Variable() to store watermark text
 
 img_frame = LabelFrame(window, text="")
 img_frame.place(x = margin, y = 40 + margin * 2, width = width, height = height)
@@ -59,14 +70,11 @@ img_frame.place(x = margin, y = 40 + margin * 2, width = width, height = height)
 img = Label(img_frame)
 img.pack()
 
-if var_image.get():
-    # render watermarked image
-    render = ImageTk.PhotoImage(data=var_image.get())
-    img.config(image = render)
-    img.image = render
-
 btn_upload_img = Button(text = "Upload Image", command = uploadImg).place(x = margin, y = margin, width = 150, height = 40)
-#btn_save = Button( text="Save", bg="green", command=add).place(x=200, y=330, width= 150 , height=40)
+
+btn_set_watermark = Button(text="Set Watermark", command = setWatermark)
+btn_set_watermark.place(x = 150 + margin * 2, y = margin, width = 150 , height = 40)
+btn_set_watermark.config(state="disabled")
 
 
 window.mainloop()
