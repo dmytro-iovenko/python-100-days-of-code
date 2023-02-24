306b4b9174964aa85a163b1f1ebc2ad0798cf769
liudmylai <liudmyla.iovenko@gmail.com>
2023-02-23 21:29:26 -0500

    define reset function

--- 85-typing-speed-test/main.py
+++ 85-typing-speed-test/main.py
@@ -11,6 +11,7 @@ def start(event):
     if not running:
         if not event.keycode in [16, 17, 18]:
             running = True
+            reset_button.config(state="normal")
             timer = window.after(100, count, 0.1)
 
     if not label.cget('text').startswith(input_entry.get()):
@@ -21,7 +22,16 @@ def start(event):
         running = False
         input_entry.config(fg="green")
 
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
+
+def reset():
+    global running
+    running = False
+    reset_button.config(state="disabled")
+    speed_label.config(text="Speed: 0.00 CPS, 0.00 CPM, 0.00 WPS, 0.00 WPM")
+    label.config(text=random.choice(text))
+    input_entry.delete(0, END)
+
+
 def count(counter):
     global running
     global timer
@@ -31,9 +41,8 @@ def count(counter):
     wps = len(input_entry.get().split(" ")) / counter
     wpm = wps * 60
 
-    speed_label.config(text=f"Speed: {cps:.2f} CPS, {cpm:.2f} CPM, {wps:.2f} WPS, {wpm:.2f} WPM")
-
     if running:
+        speed_label.config(text=f"Speed: {cps:.2f} CPS, {cpm:.2f} CPM, {wps:.2f} WPS, {wpm:.2f} WPM")
         timer = window.after(100, count, counter + 0.1)
 
 # create window
@@ -62,6 +71,11 @@ input_entry.bind("<KeyRelease>", start)
 # creating a label for the timer
 speed_label = Label(frame, text="Speed: 0.00 CPS, 0.00 CPM, 0.00 WPS, 0.00 WPM", font=("Helventica", 18))
 speed_label.grid(row=2, column=0, columnspan=2, padx=5, pady=10)
+
+# creating a reset button
+reset_button = Button(frame, text="Reset", command=reset, font=("Helventica", 24))
+reset_button.grid(row=3, column=0, columnspan=2, padx=5, pady=10)
+reset_button.config(state="disabled")
 
 frame.pack(expand=True)
 
