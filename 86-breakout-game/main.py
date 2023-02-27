cff2b6f0c912bf3f7b8b917857fe97562d33ecdb
liudmylai <liudmyla.iovenko@gmail.com>
2023-02-26 20:55:53 -0500

    creating the paddle

--- 86-breakout-game/main.py
+++ 86-breakout-game/main.py
@@ -1,4 +1,5 @@
 from turtle import Screen, Turtle
+from paddle import Paddle
 
 screen = Screen()
 screen.bgcolor("black")
@@ -6,5 +7,14 @@ screen.setup(width=800, height=600)
 screen.title("Breakout")
 screen.tracer(0)
 
+paddle = Paddle()
+ 
+screen.listen()
+screen.onkey(key='Left', fun=paddle.move_left)
+screen.onkey(key='Right', fun=paddle.move_right)
+ 
+game_is_on = True
+while game_is_on:
+    screen.update()
 
 screen.exitonclick()
