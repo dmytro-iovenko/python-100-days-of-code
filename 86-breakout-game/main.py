bf42bd3ebb7eef486a71bab444f289d2fca802fa
liudmylai <liudmyla.iovenko@gmail.com>
2023-02-28 20:55:58 -0500

    detecting collision with paddle

--- 86-breakout-game/main.py
+++ 86-breakout-game/main.py
@@ -22,18 +22,56 @@ while game_is_on:
     time.sleep(0.03)
     ball.move()
 
+    # record x-axis coordinates of ball and paddle
+    paddle_x = paddle.xcor()
+    ball_x = ball.xcor()
+    ball_y = ball.ycor()
+ 
     # detect collision with left and right walls:
-    if ball.xcor() < -380 or ball.xcor() > 370:
-        ball.bounce(x_bounce=True, y_bounce=False)
+    if ball_x < -380 or ball_x > 370:
+        ball.bounce(True, False)
  
     # detect collision with upper wall
-    elif ball.ycor() > 270:
-        ball.bounce(x_bounce=False, y_bounce=True)
+    elif ball_y > 270:
+        ball.bounce(False, True)
  
     # detect collision with bottom wall
     # In this case, user failed to hit the ball
     # thus he loses. The game resets.
-    elif ball.ycor() < -280:
+    elif ball_y < -280:
         ball.reset()
+
+    # check if ball's distance(from its middle)
+    # from paddle(from its middle) is less than
+    # width of paddle and ball is below a certain
+    # coordinate to detect their collision
+    elif ball.distance(paddle) < 110 and ball_y < -250:
+ 
+        # If Paddle is on Right of Screen
+        if paddle_x > 0:
+            if ball_x > paddle_x:
+                # If ball hits paddles left side it
+                # should go back to left
+                ball.bounce(True, True)
+            else:
+                ball.bounce(False, True)
+ 
+        # If Paddle is left of Screen
+        elif paddle_x < 0:
+            if ball_x < paddle_x:
+                # If ball hits paddles left side it
+                # should go back to left
+                ball.bounce(True, True)
+            else:
+                ball.bounce(False, True)
+ 
+        # Else Paddle is in the Middle horizontally
+        else:
+            if ball_x > paddle_x:
+                ball.bounce(True, True)
+            elif ball_x < paddle_x:
+                ball.bounce(True, True)
+            else:
+                ball.bounce(False, True)
 
 screen.exitonclick()
