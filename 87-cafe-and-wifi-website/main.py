aab5c7eb5c01b5b892e85ab558b1e11e9a0a5f31
liudmylai <liudmyla.iovenko@gmail.com>
2023-03-09 20:57:49 -0500

    render home page

--- 87-cafe-and-wifi-website/main.py
+++ 87-cafe-and-wifi-website/main.py
@@ -1,10 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+Bootstrap(app)
 
 @app.route("/")
 def home():
-    return "Cafe and Wifi Website"
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
