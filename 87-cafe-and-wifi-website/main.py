1e4aaedcc8cefb6aeec695d4ab5fcfe4147b48f0
liudmylai <liudmyla.iovenko@gmail.com>
2023-03-10 22:26:56 -0500

    render hotspots page

--- 87-cafe-and-wifi-website/main.py
+++ 87-cafe-and-wifi-website/main.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 from flask_bootstrap import Bootstrap
+import csv
 
 app = Flask(__name__)
 Bootstrap(app)
@@ -8,5 +9,15 @@ Bootstrap(app)
 def home():
     return render_template("index.html")
 
-if __name__ == '__main__':
+@app.route("/hotspots")
+def hotspots():
+    with open("free-public-wifi-hotspots.csv", newline="") as csv_file:
+        csv_data = csv.reader(csv_file, delimiter=";")
+        header_row = next(csv_data)  # get the headers
+        list_of_rows = []
+        for row in csv_data:
+            list_of_rows.append(row)
+    return render_template("hotspots.html", hotspots=list_of_rows, header=header_row)
+
+if __name__ == "__main__":
     app.run(debug=True)
