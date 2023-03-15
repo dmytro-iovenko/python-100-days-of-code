c388a5eb3376f2002d73b0fee84f77f3a65c3556
liudmylai <liudmyla.iovenko@gmail.com>
2023-03-14 21:08:29 -0400

    render the form

--- 88-todo-list/main.py
+++ 88-todo-list/main.py
@@ -1,13 +1,23 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 import csv
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "c5e1d407e79c9b506d7f48d0677d9603"
 Bootstrap(app)
 
-@app.route("/")
+class TaskForm(FlaskForm):
+    hotspot = StringField("Name ...", validators=[DataRequired()])
+    submit = SubmitField("Add task")
+
+@app.route("/", methods=["GET", "POST"])
 def home():
-    return render_template("index.html")
+    form = TaskForm()
+    return render_template("index.html", form=form)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
