a2376c7bec0ae9eee46a89bf5757d162d0e48569
liudmylai <liudmyla.iovenko@gmail.com>
2023-03-20 21:24:58 -0400

    render the form

--- 89-disappearing-text-writing-app/main.py
+++ 89-disappearing-text-writing-app/main.py
@@ -1,12 +1,19 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import TextAreaField
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "aa2e8495eae2e6e7f103e745855a926d"
 Bootstrap(app)
+
+class EditForm(FlaskForm):
+    text = TextAreaField("", render_kw={"placeholder": "Start typing...", "spellcheck": "false"})
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    form = EditForm()
+    return render_template("index.html", form=form)
 
 
 if __name__ == "__main__":
