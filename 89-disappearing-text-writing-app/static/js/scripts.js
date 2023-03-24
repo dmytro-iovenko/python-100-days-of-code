62b98c798de0954b9cc93096be43f767daaf39ee
liudmylai <liudmyla.iovenko@gmail.com>
2023-03-23 23:27:19 -0400

    counting words and styling

--- 89-disappearing-text-writing-app/static/js/scripts.js
+++ 89-disappearing-text-writing-app/static/js/scripts.js
@@ -7,6 +7,7 @@ class Writing {
         this.danger = 2;
         this.kill = 5;
         this.timerID = null;
+        this.words = '';
     }
 
     now = () => {
@@ -21,10 +22,13 @@ class Writing {
         if (!this.run) return;
         console.log(this.timeSinceStroke, this.kill)
         if (this.timeSinceStroke >= this.danger) {
-           const editor = document.getElementById('editor')
-           if (!editor.classList.contains('danger')) editor.classList.add('danger');
-           const content = document.getElementById('content')
-           if (!content.classList.contains('danger')) content.classList.add('danger');
+            const editor = document.getElementById('editor')
+            if (!editor.classList.contains('danger')) editor.classList.add('danger');
+            const body = document.body
+            if (!body.classList.contains('danger')) body.classList.add('danger');
+        } else {
+            document.getElementById('editor').classList.remove('danger');
+            document.getElementById('content').classList.remove('danger');
         }
         if (this.timeSinceStroke >= this.kill) return this.fail();
         this.timeSinceStroke += 0.1;
@@ -34,6 +38,10 @@ class Writing {
         this.run = true;
         this.startTime = this.now();
         this.timeSinceStroke = 0;
+        const text = document.getElementById('text').value;
+        const words = text.trim().length && text.trim().split(/\s+/).length;
+        this.words = words + (words === 1 ? ' word' : ' words');
+        document.getElementById('wordcount').innerHTML = this.words;
         console.log(this.timeSinceStroke, this.kill)
         if (!this.timerID) this.timerID = setInterval(() => this.tick(), 100);
     }
@@ -42,9 +50,10 @@ class Writing {
         clearInterval(this.timerID);
         this.timerID = null;
         document.getElementById('text').value = '';
+        document.body.classList.remove('danger');
         document.getElementById('editor').classList.remove('danger');
-        document.getElementById('content').classList.remove('danger');
-        setTimeout(() => alert('You lose!'), 100);
+        setTimeout(() => alert('You lose! You have written ' + this.words + '.'), 100);
+        document.getElementById('wordcount').innerHTML = '0 words';
     }
 
 }
