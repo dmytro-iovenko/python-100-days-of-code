30317c0a99523ddf8115a534fbfe5705877ec9a7
liudmylai <liudmyla.iovenko@gmail.com>
2023-03-26 22:16:23 -0400

    render pdf page in textbox

--- 90-convert-pdf-to-audiobook/main.py
+++ 90-convert-pdf-to-audiobook/main.py
@@ -1,13 +1,23 @@
 import PyPDF3
 import pyttsx3
-  
+from tkinter import *
+
 file = 'lorem.pdf'
 
+window = Tk()
+window.geometry("750x450")
+text = Text(window, width=80, height=30)
+text.pack(pady=20)
+  
 book = open(file, 'rb')
 pdfReader = PyPDF3.PdfFileReader(file)
 pdf_page = pdfReader.getPage(0)
-text = pdf_page.extractText()
+content = pdf_page.extractText()
+
+text.insert(1.0,content)
   
-speak = pyttsx3.init()
-speak.say(text)
-speak.runAndWait()
+#speak = pyttsx3.init()
+#speak.say(content)
+#speak.runAndWait()
+
+window.mainloop()
