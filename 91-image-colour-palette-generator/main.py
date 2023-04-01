63c333d30c8929d92aa4bb2758c5258df83430a7
liudmylai <liudmyla.iovenko@gmail.com>
2023-03-31 21:29:41 -0400

    render result block

--- 91-image-colour-palette-generator/main.py
+++ 91-image-colour-palette-generator/main.py
@@ -1,7 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 from PIL import Image
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "404d78ae5d0e579fa43f1d43ccb31c17"
+Bootstrap(app)
 
 @app.route("/")
 def home():
@@ -19,10 +22,9 @@ def home():
             g_total += g
             b_total += b
             count += 1
-    print()
+    result = f"{r_total/count}, {g_total/count}, {b_total/count}"
 
-    
-    return f"Most Used color is ({r_total/count}, {g_total/count}, {b_total/count})"
+    return render_template("index.html", title=f"Most Used color is ({result})", color=result)
 
 if __name__ == "__main__":
     app.run(debug=True)
