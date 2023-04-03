7ca21658a29d73dd8efdffad57742c4fb55a2c47
liudmylai <liudmyla.iovenko@gmail.com>
2023-04-02 20:13:44 -0400

    upload image with the form

--- 91-image-colour-palette-generator/main.py
+++ 91-image-colour-palette-generator/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_bootstrap import Bootstrap
 from PIL import Image
 import base64
@@ -8,30 +8,40 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = "404d78ae5d0e579fa43f1d43ccb31c17"
 Bootstrap(app)
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def home():
-    img = Image.open('dog.jpg')
 
-    data = io.BytesIO()
-    img.save(data, "JPEG")
-    encoded_img_data = base64.b64encode(data.getvalue())
+    if request.method == 'POST':
+        if "image" in request.files:
+            # get image
+            img_file = request.files["image"]
 
-    img = img.convert('RGB')
-    width, height = img.size
-    r_total = 0
-    g_total = 0
-    b_total = 0
-    count = 0
-    for x in range(0, width):
-        for y in range(0, height):
-            r, g, b = img.getpixel((x,y))
-            r_total += r
-            g_total += g
-            b_total += b
-            count += 1
-    result = f"{r_total/count}, {g_total/count}, {b_total/count}"
+            img = Image.open(img_file)
+        
+            img = img.convert('RGB')
+            width, height = img.size
+
+            data = io.BytesIO()
+            img.save(data, "JPEG")
+            encoded_img_data = base64.b64encode(data.getvalue())
+        
+            r_total = 0
+            g_total = 0
+            b_total = 0
+            count = 0
+            for x in range(0, width):
+                for y in range(0, height):
+                    r, g, b = img.getpixel((x,y))
+                    r_total += r
+                    g_total += g
+                    b_total += b
+                    count += 1
+            result = f"{round(r_total/count)}, {round(g_total/count)}, {round(b_total/count)}"
 	
-    return render_template("index.html", img_data=encoded_img_data.decode('utf-8'), title=f"Most Used color is ({result})", color=result)
+            return render_template("index.html", img_data=encoded_img_data.decode('utf-8'), title=f"Most Used color is RGB({result})", color=result)
+
+    return render_template("index.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
