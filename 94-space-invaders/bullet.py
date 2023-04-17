868fbbf00113352b724ecf6dda79ee6bd65221ac
liudmylai <liudmyla.iovenko@gmail.com>
2023-04-16 22:55:05 -0400

    creating the bullet

---
+++ 94-space-invaders/bullet.py
@@ -0,0 +1,30 @@
+from turtle import Turtle
+
+BULLET_SPEED = 10
+
+class Bullet(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.color("yellow")
+        self.shape("triangle")
+        self.penup()
+        self.y_move = BULLET_SPEED
+        self.setheading(90)
+        self.shapesize(0.5, 0.5)
+        self.reset()
+
+    def move(self):
+        new_y = self.ycor() + self.y_move
+        self.goto(self.xcor(), new_y)
+
+    def fire(self, player):
+        if self.state == "ready":
+            self.state = "fire"
+            #Move the bullet to above the player
+            self.setposition(player.xcor(), player.ycor()+10)
+            self.showturtle()
+
+    def reset(self):
+        self.hideturtle()
+        self.state = "ready"
--- 94-space-invaders/main.py
+++ 94-space-invaders/main.py
@@ -1,5 +1,6 @@
 from turtle import Screen, Turtle
 from player import Player
+from bullet import Bullet
 import time
 
 #Set up screen                                         
@@ -12,10 +13,14 @@ screen.tracer(0)
 #Create the player
 player = Player()
 
+#Create the bullet
+bullet = Bullet()
+
 #Create keyboard bindings
 screen.listen()
-screen.onkey(key='Left', fun=player.move_left)
-screen.onkey(key='Right', fun=player.move_right)
+screen.onkeypress(key='Left', fun=player.move_left)
+screen.onkeypress(key='Right', fun=player.move_right)
+screen.onkey(key='space', fun=lambda player=player: bullet.fire(player))
  
 game_is_on = True
 
@@ -24,5 +29,12 @@ while game_is_on:
     screen.update()
     time.sleep(0.01)
 
+    #Move the bullet
+    if bullet.state == "fire":
+        bullet.move()
+
+    #Check to see if the bullet has gone to the top
+    if bullet.ycor() > 275:
+        bullet.reset()
 
 screen.exitonclick()
