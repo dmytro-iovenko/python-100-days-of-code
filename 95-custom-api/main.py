9e200731b19c8d6083ec8c937e2da20a0836087b
liudmylai <liudmyla.iovenko@gmail.com>
2023-04-26 23:10:10 -0400

    add search form

--- 95-custom-api/main.py
+++ 95-custom-api/main.py
@@ -1,15 +1,36 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+import requests
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'b6f9aff3cbefb58d0cd90906864ed4a3'
 Bootstrap(app)
 
-ITBOOK_DB_SEARCH_URL = "https://api.itbook.store/1.0/"
+ITBOOK_DB_SEARCH_URL = "https://api.itbook.store/1.0/search"
+
+class FindBookForm(FlaskForm):
+    title = StringField("Book Title", validators=[DataRequired()])
+    submit = SubmitField("Add Book")
+
 
 ##RENDER HOME PAGE
 @app.route('/')
 def home():
     return render_template("index.html")
+
+
+@app.route("/add", methods=["GET", "POST"])
+def add_book():
+    form = FindBookForm()
+    if form.validate_on_submit():
+        book_title = form.title.data
+        response = requests.get(f"{ITBOOK_DB_SEARCH_URL}/query={book_title}")
+        data = response.json()["books"]
+        return render_template("index.html", options=data)
+    return render_template("add.html", form=form)
 
 
 if __name__ == "__main__":
