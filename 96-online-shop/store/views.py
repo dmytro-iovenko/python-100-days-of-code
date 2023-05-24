f4e46eafaf20efb3c774228c3b782c9d75349733
liudmylai <liudmyla.iovenko@gmail.com>
2023-05-23 22:10:56 -0400

    build the main product grid

--- 96-online-shop/store/views.py
+++ 96-online-shop/store/views.py
@@ -1,15 +1,14 @@
 from django.shortcuts import render
 
-from . models import Category
+from . models import Category, Product
 
 # Create your views here.
 
 def store(request):
-
-    return render(request, 'store/store.html')
+    all_products = Product.objects.all()
+    context = {'my_products':all_products}
+    return render(request, 'store/store.html', context)
 
 def categories(request):
-
     all_categories = Category.objects.all()
-
     return {'all_categories': all_categories}
