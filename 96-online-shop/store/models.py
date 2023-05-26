48b692ca7c65d166b0f64eb6deca53ee190be520
liudmylai <liudmyla.iovenko@gmail.com>
2023-05-25 23:19:52 -0400

    creating dynamic links

--- 96-online-shop/store/models.py
+++ 96-online-shop/store/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from django.urls import reverse
+
 # Create your models here.
 
 class Category(models.Model):
@@ -45,3 +47,8 @@ class Product(models.Model):
     def __str__(self):
 
         return self.title
+
+
+    def get_absolute_url(self):
+
+        return reverse('product-info', args=[self.slug])
