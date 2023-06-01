560926f4f2ca2d27a9997cb96418086e7dc04e99
liudmylai <liudmyla.iovenko@gmail.com>
2023-05-31 22:29:09 -0400

    add sessions

---
+++ 96-online-shop/cart/cart.py
@@ -0,0 +1,13 @@
+
+class Cart():
+
+    def __init__(self, request):
+        self.session = request.session
+        # Returning user - obtain his/her existing session
+        cart = self.session.get('session_key')
+        # New user - generate a new session
+        if 'session_key' not in request.session:
+            cart = self.session['session_key'] = {}
+        self.cart = cart
+
+
