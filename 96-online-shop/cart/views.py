e507ee6b6c2b762851de5209bfe7792a8df9cb52
liudmylai <liudmyla.iovenko@gmail.com>
2023-06-03 21:39:27 -0800

    update add functionality

--- 96-online-shop/cart/views.py
+++ 96-online-shop/cart/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .cart import Cart
 from store.models import Product
 from django.shortcuts import get_object_or_404
+from django.http import JsonResponse
 
 
 def cart_summary(request):
@@ -15,6 +16,9 @@ def cart_add(request):
         product_quantity = int(request.POST.get('product_quantity'))
         product = get_object_or_404(Product, id=product_id)
         cart.add(product=product, product_qty=product_quantity)
+        cart_quantity = cart.__len__()
+        response = JsonResponse({'qty': cart_quantity})
+        return response
 
 
 def cart_delete(request):
