e44d96a18fd792f3040b3c14be948c901b6b06b5
liudmylai <liudmyla.iovenko@gmail.com>
2023-06-13 21:47:49 -0400

    configure url files

--- 96-online-shop/ecommerce/urls.py
+++ 96-online-shop/ecommerce/urls.py
@@ -14,6 +14,8 @@ urlpatterns = [
 
     path('cart/', include('cart.urls')),
 
+    path('account/', include('account.urls')),
+
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
