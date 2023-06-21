0d3c2f7b87db0b7cd10baba0deb85ef332c4bb84
liudmylai <liudmyla.iovenko@gmail.com>
2023-06-20 22:29:38 -0400

    update view functionality

--- 96-online-shop/account/views.py
+++ 96-online-shop/account/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import redirect, render
 from .forms import CreateUserForm
+from django.contrib.sites.shortcuts import get_current_site
+from . token import user_tokenizer_generate
+from django.template.loader import render_to_string
+from django.utils.encoding import force_bytes, force_str
+from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
 
 # Create your views here.
 
@@ -8,8 +13,22 @@ def register(request):
     if request.method == 'POST':
         form = CreateUserForm(request.POST)
         if form.is_valid(): 
-            form.save()
-            return redirect('')
+            user = form.save()
+            user.is_active = False
+            user.save()
+
+            # Email verification setup (template)
+            current_site = get_current_site(request)
+            subject = 'Account verification email'
+            message = render_to_string('account/registration/email-verification.html', {
+                'user': user,
+                'domain': current_site.domain,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': user_tokenizer_generate.make_token(user),
+            })
+
+            user.email_user(subject=subject, message=message)
+            return redirect('email-verification-sent')
 
     context = {'form':form}
 
