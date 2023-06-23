f5bcd4a518e00ac8e0fc97b5db3a44c79bd9d578
liudmylai <liudmyla.iovenko@gmail.com>
2023-06-22 18:57:39 -0900

    setup email backend

--- 96-online-shop/ecommerce/settings.py
+++ 96-online-shop/ecommerce/settings.py
@@ -143,3 +143,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Email configuration settings:
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = '587'
+EMAIL_USE_TLS = 'True'
+
+EMAIL_HOST_USER = '' # - Enter GMAIL address
+EMAIL_HOST_PASSWORD = '' # - Enter app password 
