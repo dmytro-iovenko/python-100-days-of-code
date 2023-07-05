deb5d7b35133d7537f7a7ea4e7a6057f2058c333
liudmylai <liudmyla.iovenko@gmail.com>
2023-07-04 23:07:56 -0400

    creating password reset views

--- 96-online-shop/account/urls.py
+++ 96-online-shop/account/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 
 from . import views
 
+from django.contrib.auth import views as auth_views
+
+
 urlpatterns = [
 
     path('register', views.register, name='register'),
@@ -29,6 +32,20 @@ urlpatterns = [
     path('profile-management', views.profile_management, name='profile-management'),
 
     path('delete-account', views.delete_account, name='delete-account'),
+
+    # Password management urls/views
+
+    # Submit our email form
+    path('reset_password', auth_views.PasswordResetView.as_view(), name='reset_password'),
+
+    # Success message stating that a password reset email was sent
+    path('reset_password_sent', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+
+    # Password reset link
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+
+    # Success message stating that our password was reset
+    path('reset_password_complete', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 
 ]
 
