3f09d8b19f67c134bba86cf5d1574d5990779e60
liudmylai <liudmyla.iovenko@gmail.com>
2023-08-13 21:24:02 -0400

    build the checkout template

--- 96-online-shop/payment/views.py
+++ 96-online-shop/payment/views.py
@@ -2,8 +2,12 @@ from django.shortcuts import render
 
 # Create your views here.
 
+def checkout(request):
+    return render(request, 'payment/checkout.html')
+
 def payment_success(request):
     return render(request, 'payment/payment-success.html')
 
 def payment_failed(request):
     return render(request, 'payment/payment-failed.html')
+
