4578e5142a698ee1b812af1452ec0afd17c693c7
liudmylai <liudmyla.iovenko@gmail.com>
2023-08-20 16:14:23 -0400

    add checkout functionality

--- 96-online-shop/payment/views.py
+++ 96-online-shop/payment/views.py
@@ -2,8 +2,23 @@ from django.shortcuts import render
 
 # Create your views here.
 
+from . models import ShippingAddress
+
+
 def checkout(request):
-    return render(request, 'payment/checkout.html')
+    # Users with accounts -- Pre-fill the form
+    if request.user.is_authenticated:
+        try:
+            # Authenticated users WITH shipping information 
+            shipping_address = ShippingAddress.objects.get(user=request.user.id)
+            context = {'shipping': shipping_address}
+            return render(request, 'payment/checkout.html', context=context)
+        except:
+            # Authenticated users with NO shipping information
+            return render(request, 'payment/checkout.html')
+    else:
+        # Guest users
+        return render(request, 'payment/checkout.html')
 
 def payment_success(request):
     return render(request, 'payment/payment-success.html')
