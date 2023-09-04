91423f816b079d1887b477987aa477f2cf61a1ee
liudmylai <liudmyla.iovenko@gmail.com>
2023-09-04 14:30:39 -0400

    process order functionality

--- 96-online-shop/payment/views.py
+++ 96-online-shop/payment/views.py
@@ -2,7 +2,11 @@ from django.shortcuts import render
 
 # Create your views here.
 
-from . models import ShippingAddress
+from . models import ShippingAddress, Order, OrderItem
+
+from cart.cart import Cart
+
+from django.http import JsonResponse
 
 
 def checkout(request):
@@ -19,6 +23,48 @@ def checkout(request):
     else:
         # Guest users
         return render(request, 'payment/checkout.html')
+
+def complete_order(request):
+    if request.POST.get('action') == 'post':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        address1 = request.POST.get('address1')
+        address2 = request.POST.get('address2')
+        city = request.POST.get('city')
+        state = request.POST.get('state')
+        zipcode = request.POST.get('zipcode')
+        # All-in-one shipping address
+        shipping_address = (address1 + "\n" + address2 + "\n" +
+        city + "\n" + state + "\n" + zipcode
+        )
+        # Shopping cart information 
+        cart = Cart(request)
+        # Get the total price of items
+        total_cost = cart.get_total()
+        '''
+            Order variations
+            1) Create order -> Account users WITH + WITHOUT shipping information
+            2) Create order -> Guest users without an account
+        '''
+        # 1) Create order -> Account users WITH + WITHOUT shipping information
+        if request.user.is_authenticated:
+            order = Order.objects.create(full_name=name, email=email, shipping_address=shipping_address,
+            amount_paid=total_cost, user=request.user)
+            order_id = order.pk
+            for item in cart:
+                OrderItem.objects.create(order_id=order_id, product=item['product'], quantity=item['qty'],
+                price=item['price'], user=request.user)
+        # 2) Create order -> Guest users without an account
+        else:
+            order = Order.objects.create(full_name=name, email=email, shipping_address=shipping_address,
+            amount_paid=total_cost)
+            order_id = order.pk
+            for item in cart:
+                OrderItem.objects.create(order_id=order_id, product=item['product'], quantity=item['qty'],
+                price=item['price'])
+        order_success = True
+        response = JsonResponse({'success':order_success})
+        return response
 
 def payment_success(request):
     return render(request, 'payment/payment-success.html')
