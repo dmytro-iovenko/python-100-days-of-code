eae0bafb6f7865f88630fc83282338e3f4ab0deb
liudmylai <liudmyla.iovenko@gmail.com>
2023-09-22 21:24:28 -0400

    initial commit

---
+++ 97-custom-automation/README.md
@@ -0,0 +1,2 @@
+## Day 97 Project - Custom Automation
+Automate some aspect of your life using Python.
---
+++ 97-custom-automation/main.py
@@ -0,0 +1,10 @@
+'''
+Automate Excel Report:
+ - Create a Pivot Table with Python
+ - Add a Bar Chart
+ - Write Excel Formulas with Python
+ - Format Cells
+ - Convert Pivot Table to Excel Report
+'''
+
+import pandas as pd
