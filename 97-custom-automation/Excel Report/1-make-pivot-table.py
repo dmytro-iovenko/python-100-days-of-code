4f253e2605b1bcd37b069de3fdce6b3122d9fee0
liudmylai <liudmyla.iovenko@gmail.com>
2023-09-24 20:08:27 -0400

    export table to excel file

--- 97-custom-automation/Excel Report/1-make-pivot-table.py
+++ 97-custom-automation/Excel Report/1-make-pivot-table.py
@@ -1,10 +1,15 @@
 import pandas as pd
 
+
 # Read Excel File
 df = pd.read_excel('supermarket_sales.xlsx')
+
 # Select columns: 'Gender', 'Product line', 'Total'
 df = df[['Gender', 'Product line', 'Total']]
+
 # Create a Pivot Table
 pivot_table = df.pivot_table(index='Gender', columns='Product line',
                              values='Total', aggfunc='sum').round(0)
 
+# Export pivot table to Excel file
+pivot_table.to_excel('pivot_table.xlsx', 'Report', startrow=4)
