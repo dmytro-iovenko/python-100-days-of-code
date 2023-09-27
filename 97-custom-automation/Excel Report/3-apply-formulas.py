c61bce0921bf496407c26c76bed8f0ce77d188c4
liudmylai <liudmyla.iovenko@gmail.com>
2023-09-27 11:31:38 -0400

    write to excel

--- 97-custom-automation/Excel Report/3-apply-formulas.py
+++ 97-custom-automation/Excel Report/3-apply-formulas.py
@@ -10,5 +10,14 @@ min_row = wb.active.min_row
 max_row = wb.active.max_row
 
 # Write an Excel formula with Python
-sheet['B8'] = '=SUM(B6:B7)'
-sheet['B8'].style = 'Currency'
+# sheet['B8'] = '=SUM(B6:B7)'
+# sheet['B8'].style = 'Currency'
+
+# Write multiple formulas with a for loop
+for i in range(min_column+1, max_column+1):  # (B, G+1)
+    letter = get_column_letter(i)
+    sheet[f'{letter}{max_row + 1}'] = f'=SUM({letter}{min_row + 1}:{letter}{max_row})'
+    sheet[f'{letter}{max_row + 1}'].style = 'Currency'
+
+# Save workbook
+wb.save('report.xlsx')
