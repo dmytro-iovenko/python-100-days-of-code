6c1f14c9478d214975913f6b689e81ba46696632
liudmylai <liudmyla.iovenko@gmail.com>
2023-09-30 22:01:53 -0400

    finding elements

--- 97-custom-automation/Automate The News/1-news-extract-data.py
+++ 97-custom-automation/Automate The News/1-news-extract-data.py
@@ -8,3 +8,17 @@ path = './chromedriver'  # introduce path here
 driver_service = Service(executable_path=path)
 driver = webdriver.Chrome(service=driver_service)
 driver.get(web)
+
+# Finding Elements
+containers = driver.find_elements(by='xpath', value='//div[@class="teaser__copy-container"]')
+
+titles = []
+subtitles = []
+links = []
+for container in containers:
+    title = container.find_element(by='xpath', value='./a/h2').text
+    subtitle = container.find_element(by='xpath', value='./a/p').text
+    link = container.find_element(by='xpath', value='./a').get_attribute('href')
+    titles.append(title)
+    subtitles.append(subtitle)
+    links.append(link)
