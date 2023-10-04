c429c95eadd6fa74245641891ffb153e61b399c5
liudmylai <liudmyla.iovenko@gmail.com>
2023-10-03 21:00:23 -0400

    finding elements

--- 97-custom-automation/Automate The News/2-news-headless.py
+++ 97-custom-automation/Automate The News/2-news-headless.py
@@ -12,3 +12,19 @@ options.headless = True
 driver_service = Service(executable_path=path)
 driver = webdriver.Chrome(service=driver_service, options=options)
 driver.get(web)
+
+# Finding Elements
+containers = driver.find_elements(by='xpath', value='//div[@class="teaser__copy-container"]')
+
+titles = []
+subtitles = []
+links = []
+for container in containers:
+    title = container.find_element(by='xpath', value='./a/h2').text
+    subtitle = container.find_element(by='xpath', value='./a/p').text
+    link = container.find_element(by='xpath', value='./a').get_attribute('href')
+    titles.append(title)
+    subtitles.append(subtitle)
+    links.append(link)
+
+my_dict = {'title': titles, 'subtitle': subtitles, 'link': links}
