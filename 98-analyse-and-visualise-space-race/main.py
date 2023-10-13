000bca65998f1ef306d19b2b7d6ee65426617f60
Liudmyla Iovenko <86856650+liudmylai@users.noreply.github.com>
2023-10-12 23:49:52 -0400

    preliminary data exploration

--- 98-analyse-and-visualise-space-race/main.py
+++ 98-analyse-and-visualise-space-race/main.py
@@ -1,1 +1,18 @@
 import pandas as pd
+
+# Load the data
+df_data = pd.read_csv('mission_launches.csv')
+
+# Get the shape
+print("The shape of the data is: ",df_data.shape) # Gives shape of data
+
+# Get number of rows and columns
+count_row = df_data.shape[0]  # Gives number of rows
+count_col = df_data.shape[1]  # Gives number of columns
+print("There are ", count_row, " rows and ", count_col, " columns in this data.")
+
+# Get column names
+print("The column names are as follows: ",df_data.columns)
+
+# Check for NaN values or duplicates
+df_data.isna()
