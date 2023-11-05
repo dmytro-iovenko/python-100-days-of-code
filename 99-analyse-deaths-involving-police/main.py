8d4bef11783feab8543d1aa5d6cfd02a749e28c4
liudmylai <liudmyla.iovenko@gmail.com>
2023-11-04 20:17:00 -0400

    data cleaning

--- 99-analyse-deaths-involving-police/main.py
+++ 99-analyse-deaths-involving-police/main.py
@@ -1,6 +1,15 @@
 import pandas as pd
 
+# Load the Data
 data = pd.read_csv("deaths_by_police.csv")
+
+# Preliminary Data Exploration
+print(data.shape)
+data.head()
+
+# Check for Missing Values and Duplicates
+print(data.isna().values.any())
+data.duplicated().values.any()
 
 print(data.shape)
 data.head()
